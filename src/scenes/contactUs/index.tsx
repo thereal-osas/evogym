d03a04f3a1ef00187a385a7c658eead3a7aa1ece
Osas Abraham <abrahamosas3@gmail.com>
2023-11-15 11:23:14 +0000

    footer update

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -2,14 +2,143 @@ import { SelectedPage } from '@/shared/types'
 import { motion } from 'framer-motion'
 import { useForm } from 'react-hook-form'
 import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png"
+import HText from '@/shared/HText'
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void
 }
 
 const ContactUs = ({setSelectedPage}: Props) => {
+    const {
+        register, 
+        trigger,
+        formState: { errors }
+    } = useForm()
+
+    const handleSubmit = async (e: any) => {
+        const isValid = await trigger()
+        if (!isValid) {
+            e.preventDefault()
+        }
+    }
+
+    const inputStyles = `mb-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`
+
   return (
-    <div>ContactUs</div>
+    <section id='contactus' className='mx-auto w-5/6 pt-24 pb-32'>
+        <motion.div
+            onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
+        >
+            {/* Header */}
+            <motion.div
+                className='md:w-3/5'
+                initial="hidden" 
+                whileInView="visible"
+                viewport={{once: true, amount: 0.5}}
+                transition={{duration: 0.5}}
+                variants={{
+                    hidden: {opacity: 0, x: -50},
+                    visible: {opacity: 1, x: 0}
+                }}
+            >
+                <HText>
+                    <span className='text-primary-500'>JOIN NOW</span>{" "}TO GET IN SHAPE
+                </HText>
+                <p className='my-5'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, sint vero? Earum laudantium accusantium explicabo similique minus vel ratione iusto ducimus deserunt, maiores officiis quisquam!</p>
+            </motion.div>
+
+            {/* Form */}
+            <div className='mt-10 justify-between gap-8 md:flex'>
+                <motion.div
+                    className='mt-10 basis-3/5 md:mt-0'
+                    initial="hidden" 
+                    whileInView="visible"
+                    viewport={{once: true, amount: 0.5}}
+                    transition={{duration: 0.5}}
+                    variants={{
+                        hidden: {opacity: 0, y: 50},
+                        visible: {opacity: 1, y: 0}
+                }}
+                >
+                    <form
+                        target='_blank'
+                        onSubmit={handleSubmit}
+                        action='http://formsubmit.co/abrahamosas05@gmail.com'
+                        method='POST'
+                    >
+                        <input 
+                            className={inputStyles} 
+                            type="text"  
+                            placeholder='NAME' {...register("name", {
+                                required: true,
+                                maxLength: 100,
+                        })}
+                        />
+                        {errors.name && (
+                            <p className='mt-1 text-primary-500'>
+                                {errors.name.type === 'required' && 'This field is required'}
+                                {errors.name.type === 'maxLength' && 'Max length is 100 char'}
+                            </p>
+                        )}
+
+                        <input 
+                            className={inputStyles} 
+                            type="email"  
+                            placeholder='EMAIL' {...register("email", {
+                                required: true,
+                                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                        })}
+                        />
+                        {errors.email && (
+                            <p className='mt-1 text-primary-500'>
+                                {errors.email.type === 'required' && 'This field is required'}
+                                {errors.email.type === 'pattern' && 'Invalid email address'}
+                            </p>
+                        )}
+
+                        <textarea 
+                            className={inputStyles} 
+                            rows={4}
+                            cols={50} 
+                            placeholder='MESSAGE' {...register("message", {
+                                required: true,
+                                maxLength: 2000,
+                        })}
+                        />
+                        {errors.message && (
+                            <p className='mt-1 text-primary-500'>
+                                {errors.message.type === 'required' && 'This field is required'}
+                                {errors.message.type === 'maxLength' && 'Max length is 2000 char'}
+                            </p>
+                        )}
+
+                        <button
+                            type='submit'
+                            className='mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white'
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </motion.div>
+
+                <motion.div
+                    className='relative mt-16 basis-2/5 md:mt-0'
+                    initial="hidden" 
+                    whileInView="visible"
+                    viewport={{once: true, amount: 0.5}}
+                    transition={{delay: 0.3, duration: 0.5}}
+                    variants={{
+                        hidden: {opacity: 0, y: 50},
+                        visible: {opacity: 1, y: 0}
+                }}
+                >
+                    <div className='md:before:content-evolvetext w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1]'>
+                        <img src={ContactUsPageGraphic} alt="contact-us-page-graphic" className='w-full' />
+                    </div>
+                </motion.div>
+            </div>
+        </motion.div>
+    </section>
   )
 }
 
---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,30 @@
+import Logo from "@/assets/Logo.png"
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary-100 py-16">
+        <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+            <div className="mt-16 basis-1/2 md:mt-0">
+                <img src={Logo} alt="logo" />
+                <p className="my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe hic laudantium delectus aliquid quod tenetur, reiciendis id odio quidem mollitia perspiciatis voluptatibus laborum. Repellendus, veritatis?</p>
+                <p>© Evogym All Rights Reserved</p>
+            </div>
+
+            <div className="mt-16 basis-1/4 md:mt-0">
+                <h4 className="font-bold">Links</h4>
+                <p className="my-5">Massa orci senectus</p>
+                <p className="my-5">Et gravida id et etiam</p>
+                <p>Ullamcorper Vivamus</p>
+            </div>
+
+            <div className="mt-16 basis-1/4 md:mt-0">
+                <h4 className="font-bold">Contact Us</h4>
+                <p className="my-5">Tempus metus mattis risus volugate eget</p>
+                <p>(333)425-6825</p>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
